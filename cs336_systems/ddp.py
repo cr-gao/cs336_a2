c592fb0a8e8830e09a3bb1c3707a3880b18e2aa1
cr-gao <gaochenrui@sjtu.edu.cn>
2026-06-02 15:19:15 +0800

    avoid use torch.cat to reduce iter overhead

--- cs336_systems/ddp.py
+++ cs336_systems/ddp.py
@@ -1,5 +1,6 @@
 import torch
 import torch.distributed as dist
+import time
 
 class DDP(torch.nn.Module):
     def __init__(self, module):
@@ -9,8 +10,12 @@ class DDP(torch.nn.Module):
         self.rank = dist.get_rank()
         self.world_size = dist.get_world_size()
         
+        total_numel = 0
         for param in self.module.parameters():
             dist.broadcast(param.data, src=0)
+            total_numel += param.numel()
+            
+        self.flattened_grads = torch.zeros(total_numel, device=next(module.parameters()).device)
         
     def forward(self, *args, **kwargs):
         return self.module(*args, **kwargs)
@@ -23,15 +28,21 @@ class DDP(torch.nn.Module):
             dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
             param.grad.data /= self.world_size
         '''
+        grad_idx = 0
+        for param in self.module.parameters():
+            if param.grad is not None:
+                grad_size = param.grad.numel()
+                self.flattened_grads[grad_idx:grad_idx + grad_size].copy_(param.grad.view(-1))
+                grad_idx += grad_size
+
         # All-reduce once
-        flattened_grads = torch.cat([param.grad.data.view(-1) for param in self.module.parameters() if param.grad is not None])
-        dist.all_reduce(flattened_grads, op=dist.ReduceOp.SUM)
-        flattened_grads /= self.world_size
-        
+        dist.all_reduce(self.flattened_grads, op=dist.ReduceOp.SUM)
+        self.flattened_grads /= self.world_size
+
         # Distribute the reduced gradients back to each parameter
         grad_idx = 0
         for param in self.module.parameters():
             if param.grad is not None:
-                grad_size = param.grad.data.numel()
-                param.grad.data = flattened_grads[grad_idx:grad_idx + grad_size].view(param.grad.data.shape)
+                grad_size = param.grad.numel()
+                param.grad.copy_(self.flattened_grads[grad_idx:grad_idx + grad_size].view_as(param.grad))
                 grad_idx += grad_size
