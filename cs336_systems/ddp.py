41719f84dd7b758e8ca724966b2a83c84f6d5e49
cr-gao <gaochenrui@sjtu.edu.cn>
2026-06-02 14:21:15 +0800

    Minimal DDP with flat grads

--- cs336_systems/ddp.py
+++ cs336_systems/ddp.py
@@ -16,8 +16,22 @@ class DDP(torch.nn.Module):
         return self.module(*args, **kwargs)
     
     def sync_gradients(self, optimizer):
+        ''' Minimal implementation
         for param in self.module.parameters():
             if param.grad is None:
                 continue
             dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
             param.grad.data /= self.world_size
+        '''
+        # All-reduce once
+        flattened_grads = torch.cat([param.grad.data.view(-1) for param in self.module.parameters() if param.grad is not None])
+        dist.all_reduce(flattened_grads, op=dist.ReduceOp.SUM)
+        flattened_grads /= self.world_size
+        
+        # Distribute the reduced gradients back to each parameter
+        grad_idx = 0
+        for param in self.module.parameters():
+            if param.grad is not None:
+                grad_size = param.grad.data.numel()
+                param.grad.data = flattened_grads[grad_idx:grad_idx + grad_size].view(param.grad.data.shape)
+                grad_idx += grad_size
